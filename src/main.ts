d72ae23c035c85f27cc94e2b169b212039f9b39d
Lucas Cota <lucasbrunocota@live.com>
2022-10-20 11:44:08 -0300

    ba.overdrive tag.

--- src/main.ts
+++ src/main.ts
@@ -147,6 +147,7 @@ function getDescription(text: string, blackboard: Array<any>, duration: number) 
         result = result.replace(/<\$ba.dt.element>/gi, EMPTY_STRING);
         result = result.replace(/<\$ba.fragile>/gi, EMPTY_STRING);
         result = result.replace(/<\$ba.frozen>/gi, EMPTY_STRING);
+        result = result.replace(/<\$ba.overdrive>/gi, EMPTY_STRING);
         result = result.replace(/<\/>/gi, EMPTY_STRING);
         //result = result.replace(REGEX_LINEBREAK, '. ');
 
